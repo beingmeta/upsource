aaa751605c2fb637ad90fbb8a25a10c1063b9216
Ken Haase <kh@beingmeta.com>
2016-10-09 16:43:47 -0400

    Update example

--- etc/srctab.ex
+++ etc/srctab.ex
@@ -1,5 +1,5 @@
 git /tmp/framerd git@github.com:beingmeta/framerd.git SOMEFLAG=foo
-git /tmp/metabooks git@github.com:beingmeta/metabooks.git SOMEFLAG=foo
+git /tmp/metabooks git@github.com:beingmeta/metabooks.git SOMEFLAG=bar
 svn /tmp/ops svn+ssh://dev.beingmeta.com/svn/src/ops
 link /tmp/pubtool /tmp/ops/pubtool
 
