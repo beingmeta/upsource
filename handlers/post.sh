029eadb0de055f0afa471f681ddb944eeab6b293
Ken Haase <kh@beingmeta.com>
2017-01-06 12:44:48 +0000

    Make var names more specific, add handler specific post and pre scripts, and improve logging

--- handlers/post.sh
+++ handlers/post.sh
@@ -1,9 +1,12 @@
-if test ! -z "${OWNER}"; then
-   ${UPSOURCESUDO:-} chown -R ${OWNER} ${MOUNT};
+if test ! -z "${UPSOURCE_OWNER}"; then
+   echo Setting owner=${UPSOURCE_OWNER} for ${MOUNT} > 2;
+   ${UPSOURCESUDO:-} chown -R ${UPSOURCE_OWNER} ${MOUNT};
 fi;
-if test ! -z "${GROUP}"; then
-   ${UPSOURCESUDO:-} chgrp -R ${GROUP} ${MOUNT};
+if test ! -z "${UPSOURCE_GROUP}"; then
+   echo Setting group=${UPSOURCE_GROUP} for ${MOUNT} > 2;
+   ${UPSOURCESUDO:-} chgrp -R ${UPSOURCE_GROUP} ${MOUNT};
 fi;
-if test ! -z "${MODE}"; then
-   ${UPSOURCESUDO:-} chmod -R ${MODE} ${MOUNT};
+if test ! -z "${UPSOURCE_MODE}"; then
+   echo Setting mode=${UPSOURCE_MODE} for ${MOUNT} > 2;
+   ${UPSOURCESUDO:-} chmod -R ${UPSOURCE_MODE} ${MOUNT};
 fi;
