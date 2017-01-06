fad4fe7533ccf4d20959b33d10e5d6f3b8b6872d
Ken Haase <kh@beingmeta.com>
2017-01-06 12:48:22 -0500

    Add logging

--- handlers/post.sh
+++ handlers/post.sh
@@ -1,14 +1,23 @@
+if test ! -z "${UPSOURCE_TRACE}"; then
+    echo "upsource: running post.sh" >&2;
+fi;
 if test -d "${MOUNT}" || test -h "${MOUNT}"; then
     if test ! -z "${UPSOURCE_OWNER}"; then
-	echo Setting owner=${UPSOURCE_OWNER} for ${MOUNT} >&2;
+	if test ! -z "${UPSOURCE_TRACE}"; then
+	    echo Setting owner=${UPSOURCE_OWNER} for ${MOUNT} >&2;
+	fi;
 	${UPSOURCESUDO:-} chown -R ${UPSOURCE_OWNER} ${MOUNT};
     fi;
     if test ! -z "${UPSOURCE_GROUP}"; then
-	echo Setting group=${UPSOURCE_GROUP} for ${MOUNT} >&2;
+	if test ! -z "${UPSOURCE_TRACE}"; then
+	    echo Setting group=${UPSOURCE_GROUP} for ${MOUNT} >&2;
+	fi;
 	${UPSOURCESUDO:-} chgrp -R ${UPSOURCE_GROUP} ${MOUNT};
     fi;
     if test ! -z "${UPSOURCE_MODE}"; then
-	echo Setting mode=${UPSOURCE_MODE} for ${MOUNT} >&2;
+	if test ! -z "${UPSOURCE_TRACE}"; then
+	    echo Setting mode=${UPSOURCE_MODE} for ${MOUNT} >&2;
+	fi;
 	${UPSOURCESUDO:-} chmod -R ${UPSOURCE_MODE} ${MOUNT};
     fi;
 else
