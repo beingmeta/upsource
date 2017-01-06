86b8964308b541385a96ddcc5c88d88b5bd9d71d
Ken Haase <kh@beingmeta.com>
2017-01-06 11:53:37 -0500

    Make post.sh check that the mount point was created

--- handlers/post.sh
+++ handlers/post.sh
@@ -1,12 +1,17 @@
-if test ! -z "${UPSOURCE_OWNER}"; then
-   echo Setting owner=${UPSOURCE_OWNER} for ${MOUNT} > 2;
-   ${UPSOURCESUDO:-} chown -R ${UPSOURCE_OWNER} ${MOUNT};
-fi;
-if test ! -z "${UPSOURCE_GROUP}"; then
-   echo Setting group=${UPSOURCE_GROUP} for ${MOUNT} > 2;
-   ${UPSOURCESUDO:-} chgrp -R ${UPSOURCE_GROUP} ${MOUNT};
-fi;
-if test ! -z "${UPSOURCE_MODE}"; then
-   echo Setting mode=${UPSOURCE_MODE} for ${MOUNT} > 2;
-   ${UPSOURCESUDO:-} chmod -R ${UPSOURCE_MODE} ${MOUNT};
+if test -d "${MOUNT}" || test -h "${MOUNT}"; then
+    if test ! -z "${UPSOURCE_OWNER}"; then
+	echo Setting owner=${UPSOURCE_OWNER} for ${MOUNT} >&2;
+	${UPSOURCESUDO:-} chown -R ${UPSOURCE_OWNER} ${MOUNT};
+    fi;
+    if test ! -z "${UPSOURCE_GROUP}"; then
+	echo Setting group=${UPSOURCE_GROUP} for ${MOUNT} >&2;
+	${UPSOURCESUDO:-} chgrp -R ${UPSOURCE_GROUP} ${MOUNT};
+    fi;
+    if test ! -z "${UPSOURCE_MODE}"; then
+	echo Setting mode=${UPSOURCE_MODE} for ${MOUNT} >&2;
+	${UPSOURCESUDO:-} chmod -R ${UPSOURCE_MODE} ${MOUNT};
+    fi;
+else
+    echo "(stdout) The directory ${MOUNT} wasn't created!" >&1;
+    echo "(stderr) The directory ${MOUNT} wasn't created!" >&2;
 fi;
