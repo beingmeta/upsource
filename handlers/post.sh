31c49146e8a2f4522c1c48bd04a1d6abc1c1583f
Ken Haase <kh@beingmeta.com>
2022-07-25 16:35:22 +0000

    Fix upsource to handle http git urls

--- handlers/post.sh
+++ handlers/post.sh
@@ -1,24 +1,28 @@
+export UPSOURCE_OWNER=${UPSOURCE_OWNER:-${OWNER}}
+export UPSOURCE_GROUP=${UPSOURCE_GROUP:-${GROUP}}
+export UPSOURCE_FILEMODE=${UPSOURCE_FILEMODE:-${FILEMODE}}
+
 if test ! -z "${UPSOURCE_TRACE}"; then
     echo "upsource: running post.sh" >&2;
 fi;
 if test -d "${MOUNT}" || test -h "${MOUNT}"; then
-    if test ! -z "${OWNER}"; then
+    if test ! -z "${UPSOURCE_OWNER}"; then
 	if test ! -z "${UPSOURCE_TRACE}"; then
-	    echo Setting owner=${OWNER} for ${MOUNT} >&2;
+	    echo Setting owner=${UPSOURCE_OWNER} for ${MOUNT} >&2;
 	fi;
-	${UPSOURCESUDO:-} chown -R ${OWNER} ${MOUNT};
+	${UPSOURCESUDO:-} chown -R ${UPSOURCE_OWNER} ${MOUNT};
     fi;
-    if test ! -z "${GROUP}"; then
+    if test ! -z "${UPSOURCE_GROUP}"; then
 	if test ! -z "${UPSOURCE_TRACE}"; then
-	    echo Setting group=${GROUP} for ${MOUNT} >&2;
+	    echo Setting group=${UPSOURCE_GROUP} for ${MOUNT} >&2;
 	fi;
-	${UPSOURCESUDO:-} chgrp -R ${GROUP} ${MOUNT};
+	${UPSOURCESUDO:-} chgrp -R ${UPSOURCE_GROUP} ${MOUNT};
     fi;
-    if test ! -z "${FILEMODE}"; then
+    if test ! -z "${UPSOURCE_FILEMODE}"; then
 	if test ! -z "${UPSOURCE_TRACE}"; then
-	    echo Setting mode=${FILEMODE} for ${MOUNT} >&2;
+	    echo Setting mode=${UPSOURCE_FILEMODE} for ${MOUNT} >&2;
 	fi;
-	${UPSOURCESUDO:-} chmod -R ${FILEMODE} ${MOUNT};
+	${UPSOURCESUDO:-} chmod -R ${UPSOURCE_FILEMODE} ${MOUNT};
     fi;
 else
     echo "(stdout) The directory ${MOUNT} wasn't created!" >&1;
